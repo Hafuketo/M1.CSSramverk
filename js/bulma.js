c24e1b10edccb7cd1065d19773ac538c79cddded
Hafuketo <haafket@gmail.com>
2022-08-31 14:15:39 +0200

    färdigställde bulma, småfixade lite här och där

--- js/bulma.js
+++ js/bulma.js
@@ -1,3 +1,4 @@
+/* Hamburger menu code */
 const burgerIco = document.querySelector(".navbar-burger");
 const navbarMenu = document.querySelector(".navbar-menu");
 
@@ -5,3 +6,30 @@ burgerIco.addEventListener("click", () => {
 	navbarMenu.classList.toggle("is-active");
 	burgerIco.classList.toggle("is-active");
 });
+
+/* Hamburger menu code END */
+
+/* Form modal code */
+const addcatBtn = document.querySelector("#addcat-btn");
+const modalBg = document.querySelector(".modal-background");
+const xCat = document.querySelector("#x-cat");
+const cancelCat = document.querySelector("#cancel-cat");
+const modal = document.querySelector(".modal");
+
+addcatBtn.addEventListener("click", () => {
+	modal.classList.add("is-active");
+});
+
+modalBg.addEventListener("click", () => {
+	modal.classList.remove("is-active");
+});
+
+xCat.addEventListener("click", () => {
+	modal.classList.remove("is-active");
+});
+
+cancelCat.addEventListener("click", () => {
+	modal.classList.remove("is-active");
+});
+
+/* Form modal code end */
